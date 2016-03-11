9c15b01b1846543d71b596dd7dd9c55fbcbd158d
Carlos Tenorio <carlostenorioperez@hotmail.com>
2016-03-11 19:59:17 +0100

    TemplateView DatailView and ListView

--- web/views.py
+++ web/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView
+from django.views.generic import DetailView
 
 class HomePageView(TemplateView):
     template_name = "home.html"
@@ -8,23 +10,23 @@ class HomePageView(TemplateView):
 class FAQView(TemplateView):
     template_name = "faq.html"
 
-class SightingExpertCommentsView(TemplateView):
+class SightingExpertCommentsView(ListView):
     template_name = "sighting_expert_comments.html"
 
-class SightingView(TemplateView):
+class SightingView(DetailView):
     template_name = "sighting.html"
 
-class SightingsView(TemplateView):
+class SightingsView(ListView):
     template_name = "sightings.html"
 
-class SightQuestionView(TemplateView):
+class SightQuestionView(DetailView):
     template_name = "sight_question.html"
 
 class LocationsPageView(TemplateView):
     template_name = "locations.html"
 
-class SightingCommentsView(TemplateView):
+class SightingCommentsView(ListView):
 	template_name = "sighting_comments.html"
-
-class SightExpertCommentView(TemplateView):
+	
+class SightExpertCommentView(DetailView):
 	template_name = "sight_expert_comment.html"
