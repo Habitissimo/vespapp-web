5c1b1ab0e6d134c46ca0c715fcce64c6817a086e
CamPerez <camperezarevalo@hotmail.com>
2016-03-12 00:05:03 +0100

    sightin view with data

--- web/views.py
+++ web/views.py
@@ -3,9 +3,7 @@ from django.http import HttpResponse
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-
 from api.models import Sighting
-
 
 class HomePageView(TemplateView):
     template_name = "home.html"
@@ -18,12 +16,18 @@ class SightingExpertCommentsView(ListView):
 
 class SightingView(DetailView):
     template_name = "sighting.html"
+    pk_url_kwarg= "sighting_id"
+    model= Sighting
 
 class SightingsView(ListView):
     template_name = "sightings.html"
     model = Sighting# Defino el modelo que utilizo
     context_object_name = "sightings_list"# Defino la lista donde se cargan los objetos del modelo
     paginate_by = 50  # Control de la paginacion
+
+    def get_queryset(self, **kwargs):
+     	return Sighting.objects.all()
+
 
 class SightQuestionView(DetailView):
     template_name = "sight_question.html"
